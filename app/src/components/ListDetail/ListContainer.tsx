5ab7a2d92ba1fab1c83159306c5ae9ba1a38018a
Chirag Maheshwari <chirag1992m.cc@gmail.com>
2025-03-09 15:36:41 -0700

    Add to a consistent navigation behavior for different screen sizes

--- app/src/components/ListDetail/ListContainer.tsx
+++ app/src/components/ListDetail/ListContainer.tsx
@@ -14,7 +14,7 @@ export function ListContainer({
     return (
         <div
             ref={scrollContainerRef}
-            className="relative h-full max-h-screen min-h-screen w-full flex-none overflow-y-auto border-r border-gray-150 bg-white dark:border-gray-800 dark:bg-gray-900 lg:w-80 lg:bg-gray-50 lg:dark:bg-gray-900 xl:w-96"
+            className="relative h-full max-h-screen min-h-screen w-full flex-none overflow-y-auto border-r border-gray-150 bg-white dark:border-gray-800 dark:bg-gray-900 lg:bg-gray-50 lg:dark:bg-gray-900 xl:w-96"
             {...rest}
         >
             {children}
